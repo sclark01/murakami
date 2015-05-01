48af52e8328dc9ffab7da7a82bc2c83339b9b28c
sclark01 <sclark.tieche@gmail.com>
2015-04-30 23:12:59 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Murakami::Application.config.secret_key_base = 'e87a25f2c7d6d827cd79186b4a6c0b324c45ae649fbd32309603e0e31b9376b361ba031012a09f66bb7472b7768ae755512a6c18d88ff0d93cdc753364fb943a'
