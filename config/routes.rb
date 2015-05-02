639c4ed48e0c60e5acb200b59289e7ccec261df0
sclark01 <sclark.tieche@gmail.com>
2015-05-01 20:41:23 -0400

    Completed adding one item to db and retrieving it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Murakami::Application.routes.draw do
+  resources :articles
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
